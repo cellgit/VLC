20c647af34927790d55edc95ac5cc9b167ccdf25
cellgit <liuhongli2120@gmail.com>
2020-09-02 00:51:54 +0800

    [t0]创建项目

---
+++ VLCPlayer/VLCPlayer/VLCExample/VLCExampleViewController.swift
@@ -0,0 +1,22 @@
+//
+//  VLCExampleViewController.swift
+//  VLCPlayer
+//
+//  Created by 刘宏立 on 2020/9/2.
+//  Copyright © 2020 刘宏立. All rights reserved.
+//
+
+import UIKit
+
+class VLCExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .cyan
+    }
+    
+
+    
+
+}
---
+++ VLCPlayer/VLCPlayerTests/VLCPlayerTests.swift
@@ -0,0 +1,34 @@
+//
+//  VLCPlayerTests.swift
+//  VLCPlayerTests
+//
+//  Created by 刘宏立 on 2020/9/2.
+//  Copyright © 2020 刘宏立. All rights reserved.
+//
+
+import XCTest
+@testable import VLCPlayer
+
+class VLCPlayerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
